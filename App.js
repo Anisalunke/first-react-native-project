a0398db5f4d3ea0c0d3356d1843d45fe8d6122f7
Anisalunke <anicompscience@gmail.com>
2022-06-22 19:40:16 +0530

    2

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput,ScrollView} from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [people, setPeople] = useState([
+    {name:'Aniruddha', key:'1'},
+    {name:'Anurag', key:'2'},
+    {name:'Aditya', key:'3'},
+    {name:'Ajay',key:'4'},
+    {name:'Anjali',key:'5'},
+    {name:'Aryan', key:'6'},
+    {name:'baburao', key:'7'},
+
+  ]);
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ScrollView>
+      { people.map(item => (
+        <View key={item.key}>
+          <Text style={styles.item}>{ item.name }</Text>
+        </View>
+      ))}
+    </ScrollView>
+
+      
     </View>
+    
+
+    
   );
 }
 
@@ -14,7 +37,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+
+  item: {
+    backgroundColor:'pink',
+    marginTop:24,
+    padding:30,
+    fontSize:24,
+  },
+
+   
+
+  
 });
